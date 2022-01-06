23f74a14aee998e8c5aa73ad8a0b1f71280c0a4d
wws <1274704958@qq.com>
2022-01-06 19:21:38 +0800

    mirror

--- src/Sample/sky_box/main.cpp
+++ src/Sample/sky_box/main.cpp
@@ -8,7 +8,13 @@
 #include <comm_comp/scene.hpp>
 #include <generator/Generator.hpp>
 #include <sundry.hpp>
+#include <assimp/scene.h>
 #include <comm_comp/sky_box.hpp>
+
+struct Vertex
+{
+	glm::vec3 pos,normal;
+};
 
 class Quad : public vkd::SampleRender {
 public:
@@ -33,7 +39,48 @@ private:
 		cube_obj->add_comp<vkd::SkyBox>("skybox/skybox.json");
 
 		main_scene.lock()->add_child(cube_trans.lock());
+
+		auto [vertices,indices] = load_model();
 	}
+
+	std::tuple<std::shared_ptr<std::vector<Vertex>>,std::shared_ptr<std::vector<uint16_t>>>
+	load_model()
+	{
+		std::shared_ptr<Assimp::Importer> heart = gld::DefResMgr::instance()->load<gld::ResType::model>("heart_lp/heart_lp.obj");
+
+		aiNode* node = heart->GetScene()->mRootNode;
+		while(node && node->mNumMeshes <= 0 && node->mNumChildren > 0)
+		{
+			node = node->mChildren[0];
+		}
+		if(!node || node->mNumMeshes <= 0)
+			return std::make_tuple(nullptr,nullptr);
+		aiMesh* mesh = heart->GetScene()->mMeshes[ node->mMeshes[0] ];
+
+		auto vertices = std::make_shared<std::vector<Vertex>>();
+
+		for(int i = 0;i < mesh->mNumVertices;++i)
+		{
+			auto v = mesh->mVertices[i];
+			auto n = mesh->mNormals[i];
+			vertices->push_back({ 
+				.pos = glm::vec3(v.x,v.y,v.z),
+				.normal = glm::vec3(n.x,n.y,n.z)
+			});
+		}
+
+		auto indices = std::make_shared<std::vector<uint16_t>>();
+		for(int i = 0;i < mesh->mNumFaces;++i)
+		{
+			aiFace face = mesh->mFaces[i];
+			for(int j = 0;j < face.mNumIndices;++j)
+				indices->push_back(static_cast<uint16_t>(face.mIndices[j]));
+		}
+
+		return std::make_tuple(vertices,indices);
+	}
+
+	
 private:
 };
 
